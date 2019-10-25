0bd4b125200f39c492fb27898119e8fbc0f27950
yusuke <ymdysk911@gmail.com>
2019-10-25 21:17:06 +0900

    javaファイル追加

--- Seigenfunction.java
+++ Seigenfunction.java
@@ -2,7 +2,7 @@ public class sinx{
      public static void main(String[] args) {
          System.out.println("This class is to calculate sinx");
 
-         System.out.println(sin(0));
+         System.out.println(sin(0.0));
          System.out.println(sin(Math.PI/2));
          System.out.println(sin(Math.PI));
 
